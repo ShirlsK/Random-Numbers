cfbc66d6ce69c7e24d5b5f27020cc36b1813b77c
ShirlsK <sk1811kodehode@gmail.com>
2025-06-11 14:45:02 +0200

    Add files via upload

---
+++ C#RandomNumbers/randomNumb/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 31); // Random number between 1 and 30
+        int userGuess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("Welcome to 'Guess the Number'!");
+        Console.WriteLine("I have selected a number between 1 and 30. Can you guess what it is?");
+        
+        // Game loop
+        while (userGuess != numberToGuess)
+        {
+            Console.Write("Enter your guess: ");
+            
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out userGuess))
+            {
+                attempts++;
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("TOO LOW! TRY AGAIN");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("TOO HIGH! TRY AGAIN");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct! You guessed the number in {attempts} attempts.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+        Console.WriteLine("Thank you for playing!");
+    }
+}
